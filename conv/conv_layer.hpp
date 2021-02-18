e523a4620d157d70d12ff850cc2093c10cc743cf
Cuong V. Nguyen <nguyencuongcl1215@gmail.com>
2021-02-18 17:46:10 +0700

    backward()

--- conv/conv_layer.hpp
+++ conv/conv_layer.hpp
@@ -34,43 +34,88 @@ class Conv2d{
             size_t kernel_height, size_t kernel_width, size_t out_channels,
             size_t horiz_stride, size_t vertical_stride):{
             // Initialize kernels
-            for(size_t k=0; k<out_channels; k++)
-                this->kernels.push_back(arma::zeros(kernel_height, kernel_width));
+            for (size_t k=0; k<this->out_channels; k++)
+                this->kernels.push_back(arma::zeros(kernel_height, kernel_width, in_channels));
 
             // Clean up gradient
-            this->_zero_grad();
+            this->_reset_accumulated_grad();
         }
 
         arma::cube& forward(arma::cube& input){
-            assert((in_height - kernel_height) % vertical_stride == 0);
-            assert((in_width - kernel_width) % horiz_stride == 0);
+            assert((this->in_height - this->kernel_height) % this->vertical_stride == 0);
+            assert((this->in_width - this->kernel_width) % this->horiz_stride == 0);
 
             // Initialize output
             output = arma::zeros(
-                n_rows = (in_height - kernel_height)/vertical_stride + 1,
-                n_cols = (in_width - kernel_width)/horiz_stride + 1,
-                n_slices = out_channels
+                n_rows = (this->in_height - this->kernel_height)/this->vertical_stride + 1,
+                n_cols = (this->in_width - this->kernel_width)/this->horiz_stride + 1,
+                n_slices = this->out_channels
             );
 
             // Perform the convolutional operation
-            for(size_t k=0; k<out_channels; k++){
-                for (size_t i=0; i<in_height-kernel_height+1; i+=vertical_stride)
-                    for (size_t j=0; j<in_width-kernel_width+1; j+=horiz_stride)
-                        output(i/vertical_stride, j/horiz_stride, k) = arma::dot(
-                            arma::vectorise(input.subcube(i, j, 0, i+kernel_height-1, j+kernel_width-1, out_channels-1))),
+            for (size_t k=0; k<this->out_channels; k++){
+                for (size_t i=0; i<this->in_height-this->kernel_height+1; i+=this->vertical_stride)
+                    for (size_t j=0; j<this->in_width-this->kernel_width+1; j+=this->horiz_stride)
+                        output(i/this->vertical_stride, j/this->horiz_stride, k) = arma::dot(
+                            arma::vectorise(input.subcube(i, j, 0, i+this->kernel_height-1, j+this->kernel_width-1, this->in_channels-1))),
                             arma::vectorise(this->kernels[k]);
             }
             this->input = input;
-            this->output = output;
             return output;
         }
 
-        void backward(arma::cube& gradient){
-        }
-    
-    private:
-        void _zero_grad(){
+        void backward(arma::cube& upstream_gradient, arma::cube& output){
+            // Initialize input gradient
+            this->grad_input = arma::zeros(arma::size(input))
+
+            // Compute gradient wrt input
+            for (size_t k=0; k<this->out_channels; k++)
+                for (size_t i=0; i<output.n_rows; i++)
+                    for (size_t j=0; j<output.n_cols; j++){
+                        arma::cube tmp(arma::size(input), arma::fill:zeros);
+                        tmp.subcube(i*this->vertical_stride, 
+                                    j*this->horiz_stride, 
+                                    0,
+                                    i*this->vertical_stride + this->kernel_height-1, 
+                                    j*this->horiz_stride + this->kernel_width-1, 
+                                    this->in_channels-1)
+                        = this->kernels[k];
+
+                        this->grad_input += upstream_gradient.slice(k)(i, j) * tmp;
+                    }
             
+            this->accumulated_grad_input += this->grad_input;
+
+            // Initialize gradient wrt filters
+            this->grad_kernels.clear();
+            this->grad_kernels.resize(this->out_channels);
+            for (size_t k=0; k<this->out_channels; k++)
+                this->grad_kernels[k] = arma::zeros(this->kernel_height, this->kernel_width, this->in_channels);
+            
+            // Compute gradient wrt filters
+            for (size_t k=0; k<this->out_channels; k++)
+                for (size_t i=0; i<output.n_rows; i++)
+                    for (size_t j=0; j<output.n_cols; j++){
+                        arma::cube tmp(arma::size(kernels[k], arma::fill:zeros));
+                        tmp = this->input.subcube(i*this->vertical_stride, 
+                                                  j*this->horiz_stride, 
+                                                  0,
+                                                  i*this->vertical_stride + this->kernel_height-1, 
+                                                  j*this->horiz_stride + this->kernel_width-1,
+                                                  this->in_channels-1);
+                        
+                        this->grad_kernels[k] += upstream_gradient.slice(k)(i, j) * tmp;
+                    }
+            
+            for (size_t k=0; k<this->out_channels; k++)
+                this->accumulated_grad_kernels[k] += this->grad_kernels[k]
+
+        }
+
+    private:
+        void _reset_accumulated_grad(){
+            this->accumulated_grad_kernels.clear();
+
         }
 }
 
