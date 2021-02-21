614d574ba355692259a68989a3b41e0e2ea57ad8
Cuong V. Nguyen <nguyencuongcl1215@gmail.com>
2021-02-21 16:29:59 +0700

    add 1d conv

--- conv/conv_layer.hpp
+++ conv/conv_layer.hpp
@@ -1,92 +1,37 @@
-#include <iostream>
-#include <cstdlib>
+#include<vector>
 
 using namespace std;
 
-// Regular convolution function.
-void convolute(int ** output, int ** input, int ** kernel)
-{
-    int convolute = 0; // This holds the convolution results for an index.
-    int x, y; // Used for input matrix index
+// 1D convolution function.
+vector<vector<double>>& convolute(vector<vector<double>> input, vector<vector<double>> kernel)
+{   
+    int input_height = input.size();
+    int input_width = input[0].size();
+    int kernel_height = kernel.size();
+    int kernel_width = kernel[0].size();
+
+    int output_height = input_height - kernel_height + 1;
+    int output_width = input_width - kernel_width + 1;
+
+    static vector<vector<double>> output;
+    for (int i=0; i<output_height; i++)
+        output.push_back(vector<double> (output_width, 0.0));
+
+    double convolute = 0; // This holds the convolution results for an index.
 
 	// Fill output matrix: rows and columns are i and j respectively
-	for (int i = 0; i < 6; i++)
-	{
-		for (int j = 0; j < 6; j++)
-		{
-			x = i;
-			y = j;
+    for (int i=0; i<output_height; i++){
+        for (int j=0; j<output_width; j++){
 
-			// Kernel rows and columns are k and l respectively
-			for (int k = 0; k < 3; k++)
-			{
-				for (int l = 0; l < 3; l++)
-				{
-					// Convolute here.
-					convolute += kernel[k][l] * input[x][y];
-					y++; // Move right.
-				}
-				x++; // Move down.
-				y = j; // Restart column position
-			}
-			output[i][j] = convolute; // Add result to output matrix.
-			convolute = 0; // Needed before we move on to the next index.
-		}
-	}
-}
-
-int main(int argc, char * argv[])
-{
-
-    int ** kernel = new int*[3];
-
-    for(int i = 0; i < 3; ++i)
-    {
-        kernel[i] = new int[3];
-    }
-
-    for(int i = 0; i < 3; i++)
-    {
-        for(int j = 0; j < 3; j++)
-            kernel[i][j] = 1;
-    }
-
-    int ** matrixIn = new int*[8];
-    for(int i = 0; i < 8; ++i)
-    {
-        matrixIn[i] = new int[8];
-    }
-
-    for(int i = 0; i < 8; i++)
-    {
-        for(int j = 0; j < 8; j++)
-            matrixIn[i][j] = 1;
-    }
-
-    int ** output = new int*[6];
-    for(int i = 0; i < 6; ++i)
-    {
-        output[i] = new int[6];
-    }
-
-    for(int i = 0; i < 6; i++)
-        output[i][i] = 0;
-
-    convolute(output, matrixIn, kernel);
-
-    // Print out the resulting matrix.
-    for(int i = 0; i < 6; i++)
-    {
-        for(int j = 0; j < 6; j++)
-        {
-            cout << output[i][j] << " ";
+            for (int k=0; k<kernel_height; k++){
+                for (int l=0; l<kernel_width; l++){
+                    convolute += input[i+k][j+l] * kernel[k][l];
+                }
+            }
+            output[i][j] = convolute;
+            convolute = 0;
         }
-        cout << endl;
     }
 
-    delete[] kernel;
-    delete[] matrixIn;
-    delete[] output;
-
-    return 0;
+    return output;
 }
