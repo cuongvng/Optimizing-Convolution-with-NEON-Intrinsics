9553c868ef39dc2b44d4f0c8609acb0afe2705c9
Cuong V. Nguyen <nguyencuongcl1215@gmail.com>
2021-02-25 17:58:31 +0700

    compare NEON

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,21 @@ int main()
   vector<vector<float32_t>> input({{65,66,67, 65, 40}, {68,69,70, 68, 40}, {71,72,73, 71, 40}, {74,75,76,74, 40}});
   vector<vector<float32_t>> kernel({{65,66,67}, {68,69,70}, {71,72,73}});
 
-  vector<vector<float32_t>> res = convolve_neon(input, kernel);
-  for (int i=0; i<res.size(); i++)
-    for (int j=0; j<res[0].size(); j++)
-      cout << res[i][j] << '\t';
-  
+  cout << "None-NEON results:" << endl;
+  vector<vector<float32_t>> non_neon = simply_convolve(input, kernel);
+  for (int i=0; i<non_neon.size(); i++){
+    for (int j=0; j<non_neon[0].size(); j++)
+      cout << non_neon[i][j] << '\t';
+    cout << endl;
+  }
+
+  cout << "NEON results:" << endl;
+  vector<vector<float32_t>> neon = simply_convolve_neon(input, kernel);
+  for (int i=0; i<neon.size(); i++){
+    for (int j=0; j<neon[0].size(); j++)
+      cout << neon[i][j] << '\t';
+    cout << endl;
+  }
 }
 
 
